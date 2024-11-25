a2ded6502cefc4b1629b3a3e7dabb6342cca8361
JaxNext <qianjunyingggo@gmail.com>
2024-11-25 16:24:30 +0800

    add token

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { checkSummarizerUsability } from '@rejax/browser-ai'
+import { checkSummarizerUsability, setOriginTrialToken } from '@rejax/browser-ai'
 import DarkMode from '@/components/DarkMode.vue'
 import NotSupported from '@/components/NotSupported.vue'
 import Summarizer from '@/components/Summarizer.vue'
+import { SUMMARIZER_ORIGIN_TRIAL_TOKEN } from '@/constants/trial'
 
 const isSupported = ref(false)
 
 onMounted(async () => {
   const res = await checkSummarizerUsability()
   isSupported.value = res.available
+  if (res.available && SUMMARIZER_ORIGIN_TRIAL_TOKEN) {
+    setOriginTrialToken(SUMMARIZER_ORIGIN_TRIAL_TOKEN)
+  }
 })
 </script>
 
